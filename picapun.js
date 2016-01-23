54a952bb9f62c4bae091ee8d8ea1c922b06fa849
Okma <c.amko@ufl.edu>
2016-01-23 16:33:19 -0500

    Funny Business

--- picapun.js
+++ picapun.js
@@ -1,17 +1,16 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.main.helpers({
+    selectedImage: function () {
+      return Session.get('imageSrc');
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.main.events({
+    'submit #submit-img': function (event) {
+      event.preventDefault();
+      console.log(event.target);
+      Session.set('imageSrc', event.target);
     }
   });
 }
@@ -19,5 +18,6 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    BlazeLayout.render('body', {main: "noImg"});
   });
 }
